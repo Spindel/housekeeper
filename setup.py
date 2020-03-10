4db2845b0d86635a6cabf9df9e6b261bd1dfa43b
D.S. Ljungmark <ljungmark@modio.se>
2020-03-10 15:56:12 +0100

    build: Use setuptools_scm for version handling
    It means I dont have to track version and update setup.py every time.

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ requires = ["psycopg2-binary >= 2.7.4"]
 
 setup(
     name="housekeeper",
-    version="1.0",
     description="A Zabbix housekeeper for Modio",
     long_description="",
     author="D.S. Ljungmark",
@@ -16,6 +15,7 @@ setup(
     zip_safe=True,
     include_package_data=True,
     install_requires=requires,
+    use_scm_version={"write_to": "version.txt"},
     entry_points={
         "console_scripts": [
             "housekeeper = housekeeper.housekeeper:main",
