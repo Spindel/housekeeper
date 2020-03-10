61002eed6db2701b5e317b61d3ce1063e7306987
D.S. Ljungmark <ljungmark@modio.se>
2020-03-10 15:51:19 +0100

    build: Drop the deprecated "setup.py test" invocation
    Since that is deprecated, there is little need for "pip install .[testing]"
    anymore, so all build dependencies have been moved into requirements-dev.txt
    
    Therefore, the config options have been moved into setup.cfg, and the
    .gitlab-ci.yml file has been updated to better cover things
    
    Meanwhile, I also added test reports and coverage handling.

--- setup.py
+++ setup.py
@@ -1,63 +1,25 @@
-import sys
-
 from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
 
-requires = [
-    'psycopg2-binary >= 2.7.4'
-]
-
-setup_requires = [
-    'flake8',
-]
-
-tests_require = [
-    'pytest',
-]
-
-
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        super().initialize_options()
-        self.pytest_args = []
-
-    def finalize_options(self):
-        super().finalize_options()
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        # import here, so the egg files have been loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
-
+requires = ["psycopg2-binary >= 2.7.4"]
 
 setup(
-    name='housekeeper',
-    version='0.2',
-    description='A Zabbix housekeeper for Modio',
-    long_description='',
-    author='D.S. Ljungmark',
-    author_email='spider@modio.se',
-    url='https://www.modio.se',
+    name="housekeeper",
+    version="1.0",
+    description="A Zabbix housekeeper for Modio",
+    long_description="",
+    author="D.S. Ljungmark",
+    author_email="spider@modio.se",
+    url="https://www.modio.se",
     packages=find_packages(),
     zip_safe=True,
     include_package_data=True,
     install_requires=requires,
-    setup_requires=setup_requires,
-    extras_require={
-        'testing': tests_require,
-    },
-    cmdclass={'test': PyTest},
     entry_points={
-        'console_scripts': [
-            'housekeeper = housekeeper.housekeeper:main',
-            'retention = housekeeper.retention:main',
-            'partition = housekeeper.partition:main',
-            'archiver = housekeeper.archiver:main',
+        "console_scripts": [
+            "housekeeper = housekeeper.housekeeper:main",
+            "retention = housekeeper.retention:main",
+            "partition = housekeeper.partition:main",
+            "archiver = housekeeper.archiver:main",
         ]
     },
 )
